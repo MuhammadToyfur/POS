526b75f7cf6aabae97a50b4dfd2f2a82e9edc89c
MuhammadToyfur <muhammadtoyfurditaramadan@gmail.com>
2024-09-06 21:18:35 +0700

    Pertemuan 2 - Soal Praktikum

---
+++ resources/views/products/user.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Profil Pengguna</title>
+</head>
+<body>
+    <h1>Profil Pengguna</h1>
+    <p>2241760126: {{ $id }}</p>
+    <p>Muhammad Toyfur Dita Ramadan: {{ $name }}</p>
+</body>
+</html>
